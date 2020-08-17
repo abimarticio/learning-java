26f953f5f0873f493ff59bed194f533f0223c7df
Abi <marticiobgl@gmail.com>
2020-08-17 22:10:07 +0800

    chore: add sample on declaring data types

---
+++ samples/Sample_2.java
@@ -0,0 +1,32 @@
+// Data types
+// Primitive data types
+// byte, short, int, long, float, double, boolean, char
+// Non-primitive, string
+
+public class Sample_2 {
+    public static void main(String[] args) {
+        String text = "This is a text.";
+        char letter = 'A';
+        boolean x   = true;
+        
+        // Integer types
+        byte bNum = 127;
+        short sNum     = 1000;
+        int iNum = 200000;
+        long lNum = 300000000000L;
+
+        // Floating point types
+        float fNum   = 2.0f;
+        double dNum = 18.24d;
+
+        System.out.println(text);
+        System.out.println(letter);
+        System.out.println(x);
+        System.out.println(bNum);
+        System.out.println(sNum);
+        System.out.println(iNum);
+        System.out.println(lNum);
+        System.out.println(fNum);
+        System.out.println(dNum);
+    }
+}
