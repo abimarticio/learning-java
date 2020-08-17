04ae0f84a21a19f1871814fe560e7ac1a3721697
Abi <marticiobgl@gmail.com>
2020-08-17 22:07:04 +0800

    chore: add output sample

---
+++ samples/Sample_1.java
@@ -0,0 +1,14 @@
+// Class name must match the filename
+// print "Hello World"
+
+public class Sample_1 {
+    public static void main(String[] args) {
+        
+        // prints string inside the quotes but cursor moves to the beggining of the next line.
+        System.out.println("Hello World");
+
+        //prints string inside the quotes.
+        System.out.print("Hello World");
+        System.out.printf("Hello World");
+    }
+}
