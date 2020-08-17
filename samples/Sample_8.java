2be1fc772b9d62832f045b53df1da637ca2b3d6c
Abi <marticiobgl@gmail.com>
2020-08-17 22:28:29 +0800

    chore: add sample on for loop

---
+++ samples/Sample_8.java
@@ -0,0 +1,10 @@
+// for loop
+
+public class Sample_8 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            i += 1;
+            System.out.println(i); // outputs odd numbers from 0 to 10
+        }
+    }
+}
