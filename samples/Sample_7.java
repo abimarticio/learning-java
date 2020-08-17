32837ad8ed06a66049a43f074354462210a1d632
Abi <marticiobgl@gmail.com>
2020-08-17 22:27:35 +0800

    chore: add sample on switch statements

---
+++ samples/Sample_7.java
@@ -0,0 +1,33 @@
+// Switch statements
+
+import java.util.Scanner; 
+
+public class Sample_7 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter command: ");
+        String command = input.nextLine();
+        input.close();
+
+
+        switch (command) {
+            
+            case "on":
+            System.out.println("Lights are now turned on.");
+            break;
+
+            case "off":
+            System.out.println("Lights are now turned off.");
+            break;
+
+            case "dim":
+            System.out.println("Lights dimmed.");
+            break;
+
+            default:
+            System.out.println("Cannot recognize the command you entered.");
+        }
+    }
+}
