6aadf0bc3207970f713aa5a78825ce51be156ba9
Abi <marticiobgl@gmail.com>
2020-08-21 21:06:49 +0800

    chore: add exercise on arithmetic operations

---
+++ exercises/Exercise_2.java
@@ -0,0 +1,53 @@
+// ask for value of x and y
+// perform the following operations
+// addition, subtraction, multiplication, division and modulo
+
+import java.util.Scanner;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Addition");
+        System.out.print("Value for x: " );
+        int x = input.nextInt();
+        System.out.print("Value for y: ");
+        int y = input.nextInt();
+        int z = x + y;
+        System.out.println("x + y = " + z);
+
+        System.out.println("Subtraction");
+        System.out.print("Value for x: " );
+        x = input.nextInt();
+        System.out.print("Value for y: ");
+        y = input.nextInt();
+        z = x - y;
+        System.out.println("x - y = " + z);
+
+        System.out.println("Multiplication");
+        System.out.print("Value for x: " );
+        x = input.nextInt();
+        System.out.print("Value for y: ");
+        y = input.nextInt();
+        z = x * y;
+        System.out.println("x * y = " + z);
+
+        System.out.println("Division");
+        System.out.print("Value for x: " );
+        x = input.nextInt();
+        System.out.print("Value for y: ");
+        y = input.nextInt();
+        z = x / y;
+        System.out.println("x / y = " + z);
+
+        System.out.println("Modulo");
+        System.out.print("Value for x: " );
+        x = input.nextInt();
+        System.out.print("Value for y: ");
+        y = input.nextInt();
+        z = x % y;
+        System.out.println("x % y = " + z);
+        input.close();
+    }
+}
