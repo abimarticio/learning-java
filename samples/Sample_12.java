06bc75930dd75c3e2ed243380f8e5266d542969b
Abi <marticiobgl@gmail.com>
2020-08-24 20:19:03 +0800

    chore: add sample on multidimensional array

---
+++ samples/Sample_12.java
@@ -0,0 +1,17 @@
+// multidimentional arrays
+
+public class Sample_12 {
+    public static void main(String[] args) {
+        int[][] num = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+        System.out.println(num[1][2]); // outputs 6
+        System.out.println(num[2][0]); // outputs 7
+        System.out.println(num[0][0]); // outputs 1
+
+        for(int i = 0; i < num.length; i++) {
+            for(int j = 0; j < num[i].length; j++) {
+                System.out.println(num[i][j] + " "); 
+            }
+        } 
+    }
+}
