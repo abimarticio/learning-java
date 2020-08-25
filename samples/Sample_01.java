53aa75de81dab4b49c1e3b86b6989202a523f179
Abi <marticiobgl@gmail.com>
2020-08-26 02:49:17 +0800

    refactor: rename class

--- samples/Sample_01.java
+++ samples/Sample_01.java
@@ -1,7 +1,7 @@
 // Class name must match the filename
 // print "Hello World"
 
-public class Sample_1 {
+public class Sample_01 {
     public static void main(String[] args) {
         
         // prints string inside the quotes but cursor moves to the beggining of the next line.
