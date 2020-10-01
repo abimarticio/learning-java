52f8ae68d54ddc8e0bc9e90e88766b1242e0dfbc
Abi <marticiobgl@gmail.com>
2020-10-02 00:05:57 +0800

    chore: add conditions to access the methods

--- exercises/Exercise_09.java
+++ exercises/Exercise_09.java
@@ -22,5 +22,30 @@ public class Exercise_09 {
     public static void main(String args[]) {
         Exercise_09 obj = new Exercise_09();
         Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("Do you want to encrypt or decrypt: ");
+            String mode = sc.nextLine().toUpperCase();
+            if (mode.equals("ENCRYPT")) {
+                System.out.println("Enter text you want to encrypt: ");
+                String textToEncrypt = sc.nextLine().toUpperCase();
+                System.out.println("Key: ");
+                int key = sc.nextInt();
+                obj.encryptText(textToEncrypt, key);
+                break;
+            }
+            else if (mode.equals("DECRYPT")) {
+                System.out.println("Enter text you want to decrypt: ");
+                String textToDecrypt = sc.nextLine().toUpperCase();
+                System.out.println("Key: ");
+                int key = sc.nextInt();
+                obj.decryptText(textToDecrypt, key);
+                break;
+            }
+            else {
+                continue;
+            }
+            
+        }
+        sc.close();
     }
 }
