bdd6d58b2f2431e5d171cc8c15ec7e5c2e4a5577
Abi <marticiobgl@gmail.com>
2020-10-01 23:49:33 +0800

    chore: add method to decrypt text

--- exercises/Exercise_09.java
+++ exercises/Exercise_09.java
@@ -1,5 +1,12 @@
 // Ceasar cipher
 
 public class Exercise_09 {
-
+    
+    void decryptText(String textToDecrypt, int key) {
+        String result = "";
+        for(int i = 0; i < textToDecrypt.length(); i++) {
+            result +=  (char)(((int)textToDecrypt.charAt(i) - key - 65) % 26 + 65);
+        }
+        System.out.println(result);
+    }
 }
