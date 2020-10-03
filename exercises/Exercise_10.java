1f93dd8dbc89fe2ce7ef2be61a55d94620839af5
Abi <marticiobgl@gmail.com>
2020-10-03 23:50:19 +0800

    chore: add method to encrypt or decrypt text

--- exercises/Exercise_10.java
+++ exercises/Exercise_10.java
@@ -1,5 +1,11 @@
 // Atbash
 
 public class Exercise_10 {
-    
+    void encryptDecryptText(String userInput) {
+        String result = "";
+        for(int i = 0; i < userInput.length(); i++) {
+            result += (char)(((int)userInput.charAt(i) * 25 + 25 - 65) % 26 + 65); 
+        }
+        System.out.println(result);
+    }
 }
