254f35e425f5f60aec4dd2aa1ac78d24d7adad9a
Abi <marticiobgl@gmail.com>
2020-10-05 00:01:36 +0800

    chore: add compute series method

--- exercises/Exercise_11.java
+++ exercises/Exercise_11.java
@@ -36,5 +36,15 @@ class Exponential extends TaylorSeries {
     }
 }
 class Sine extends TaylorSeries {
-   
+    static double computeSeries(double x, double numTerms) {
+        x = Math.toRadians(x);
+        double approximation = 0;
+        for (int i = 0; i < numTerms; i++) {
+            double coefficient = Math.pow((-1), i);
+            double numerator = Math.pow(x, (2 * i + 1));
+            double denominator = factorial(2 * i + 1);
+            approximation += (coefficient * (numerator / denominator));
+        }
+        return approximation;
+    }
 }
