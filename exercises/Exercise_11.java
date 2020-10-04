58fd88815087b994b8574efc9b781503dbfcbf07
Abi <marticiobgl@gmail.com>
2020-10-04 23:56:47 +0800

    chore: add compute series method

--- exercises/Exercise_11.java
+++ exercises/Exercise_11.java
@@ -1,5 +1,5 @@
 // Taylor Series
-
+import java.lang.Math;
 
 class TaylorSeries {
     static int factorial(int num) {
@@ -12,5 +12,15 @@ class TaylorSeries {
     }
 }
 class Cosine extends TaylorSeries {
-    
+    static double computeSeries(double x, double numTerms) {
+        x  = Math.toRadians(x);
+        double approximation = 0;
+        for (int i = 0; i < numTerms; i++) {
+            double coefficient = Math.pow((-1), i);
+            double numerator = Math.pow(x, (2 * i));
+            double denominator = factorial(2 * i);
+            approximation += (coefficient * (numerator / denominator));
+        }
+        return approximation;
+    } 
 }
