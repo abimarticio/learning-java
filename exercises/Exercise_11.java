fc2be4020ce43c34f905425a5d5ca268ea1a54d3
Abi <marticiobgl@gmail.com>
2020-10-04 23:53:53 +0800

    chore: add factorial method

--- exercises/Exercise_11.java
+++ exercises/Exercise_11.java
@@ -2,5 +2,12 @@
 
 
 class TaylorSeries {
-  
+    static int factorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        else{
+            return num * factorial(num - 1);
+        }
+    }
 }
