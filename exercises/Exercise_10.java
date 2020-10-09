67b0bc6f6fbc247fc28b74198f3f8dcfdb7ea6fd
Abi <marticiobgl@gmail.com>
2020-10-09 22:42:39 +0800

    chore: convert void method to static string method

--- exercises/Exercise_10.java
+++ exercises/Exercise_10.java
@@ -2,12 +2,12 @@
 import java.util.Scanner;
 
 public class Exercise_10 {
-    void encryptDecryptText(String userInput) {
+    Static String encryptDecryptText(String userInput) {
         String result = "";
         for(int i = 0; i < userInput.length(); i++) {
             result += (char)(((int)userInput.charAt(i) * 25 + 25 - 65) % 26 + 65); 
         }
-        System.out.println(result);
+        return result;
     }
     public static void main(String args[]) {
         Exercise_10 obj = new Exercise_10();
