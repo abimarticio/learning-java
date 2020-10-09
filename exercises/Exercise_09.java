c96dd8b91e7f3d6c7fd2ce20faba11fd93ed9f9a
Abi <marticiobgl@gmail.com>
2020-10-09 22:15:07 +0800

    chore: retrieve function returns

--- exercises/Exercise_09.java
+++ exercises/Exercise_09.java
@@ -20,7 +20,6 @@ public class Exercise_09 {
     }
 
     public static void main(String args[]) {
-        Exercise_09 obj = new Exercise_09();
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.println("Do you want to encrypt or decrypt: ");
@@ -30,7 +29,8 @@ public class Exercise_09 {
                 String textToEncrypt = sc.nextLine().toUpperCase();
                 System.out.println("Key: ");
                 int key = sc.nextInt();
-                obj.encryptText(textToEncrypt, key);
+                String encryptedText = encryptText(textToEncrypt, key);
+                System.out.println(encryptedText);
                 break;
             }
             else if (mode.equals("DECRYPT")) {
@@ -38,7 +38,8 @@ public class Exercise_09 {
                 String textToDecrypt = sc.nextLine().toUpperCase();
                 System.out.println("Key: ");
                 int key = sc.nextInt();
-                obj.decryptText(textToDecrypt, key);
+                String decryptedText = decryptText(textToDecrypt, key);
+                System.out.println(decryptedText);
                 break;
             }
             else {
